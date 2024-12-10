042fd2b4cd4977fcf79c21749b0308cf552271e8
pyrsson <55557230+pyrsson@users.noreply.github.com>
2024-12-10 19:19:45 +0100

    helm extra

--- lazyvim/dot-config/nvim/lua/config/lazy.lua
+++ lazyvim/dot-config/nvim/lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.helm" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.omnisharp" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
