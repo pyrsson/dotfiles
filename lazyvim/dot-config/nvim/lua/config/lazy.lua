8f65777362d55e9552bbfa9304343940448c6153
pyrsson <55557230+pyrsson@users.noreply.github.com>
2025-01-18 10:11:00 +0100

    add nix extra

--- lazyvim/dot-config/nvim/lua/config/lazy.lua
+++ lazyvim/dot-config/nvim/lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.helm" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.omnisharp" },
+    { import = "lazyvim.plugins.extras.lang.nix" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
