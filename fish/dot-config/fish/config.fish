acbdd78790c84413a83e31e245b6ab0692751bd1
pyrsson <55557230+pyrsson@users.noreply.github.com>
2025-01-24 10:40:30 +0100

    zellij config

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -9,7 +9,23 @@ if status is-interactive
     type -q direnv && direnv hook fish | source
     type -q fzf && fzf --fish | source
     # makes fzf history preview open in tmux popup
-    set -gx FZF_DEFAULT_OPTS "--reverse --tmux 80%,50% --color dark,prompt:blue,hl+:cyan,hl:cyan,bg+:gray,gutter:-1,fg+:blue:bold,pointer:cyan,info:blue,border:gray"
+    set -gx FZF_DEFAULT_OPTS "--reverse --tmux 80%,50% --color=bg+:#2d3f76 \
+    --color=bg:#1e2030 \
+    --color=border:#589ed7 \
+    --color=fg:#c8d3f5 \
+    --color=gutter:#1e2030 \
+    --color=header:#ff966c \
+    --color=hl+:#65bcff \
+    --color=hl:#65bcff \
+    --color=info:#545c7e \
+    --color=marker:#ff007c \
+    --color=pointer:#ff007c \
+    --color=prompt:#65bcff \
+    --color=query:#c8d3f5:regular \
+    --color=scrollbar:#589ed7 \
+    --color=separator:#ff966c \
+    --color=spinner:#ff007c \
+    "
     set -gx FZF_CTRL_R_OPTS "--reverse --preview 'echo {}' --preview-window down:3:wrap:hidden:border-horizontal --bind '?:toggle-preview'"
     set -gx FZF_CTRL_T_OPTS "--preview 'test -d {} && eza -la --color=always {} || bat --style=numbers --color=always --line-range=:500 {} 2> /dev/null | head -200'"
 
@@ -20,6 +36,7 @@ if status is-interactive
     type -q cue && cue completion fish | source
     type -q kcl && kcl completion fish | source
     type -q hcp && hcp completion fish | source
+    type -q zellij && zellij setup --generate-completion fish | source
 
     # alias
     type -q eza && alias ls=eza
