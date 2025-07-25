e2012b1e1554fe328bf9ef4d8ed6f4f79ed3c2cd
pyrsson <55557230+pyrsson@users.noreply.github.com>
2025-07-25 09:48:15 +0200

    cleanup

--- fish/dot-config/fish/functions/setup-extras.fish
+++ fish/dot-config/fish/functions/setup-extras.fish
@@ -1,13 +1,13 @@
 function setup-extras -d 'setup extras'
-    type -q kustomize && kustomize completion fish >$XDG_CONFIG_HOME/fish/completions/kustomize.fish
-    type -q talosctl && talosctl completion fish >$XDG_CONFIG_HOME/fish/completions/talosctl.fish
-    type -q stern && stern --completion fish >$XDG_CONFIG_HOME/fish/completions/stern.fish
-    type -q cue && cue completion fish >$XDG_CONFIG_HOME/fish/completions/cue.fish
-    type -q kcl && kcl completion fish >$XDG_CONFIG_HOME/fish/completions/kcl.fish
-    type -q hcp && hcp completion fish >$XDG_CONFIG_HOME/fish/completions/hcp.fish
-    type -q zellij && zellij setup --generate-completion fish >$XDG_CONFIG_HOME/fish/completions/zellij.fish
-    type -q vcluster && vcluster completion fish >$XDG_CONFIG_HOME/fish/completions/vcluster.fish
-    type -q argocd && argocd completion fish >$XDG_CONFIG_HOME/fish/completions/argocd.fish
+    # standard style command completion
+    for e in kustomize talosctl cue kcl hcp vcluster argocd kcp kubebuilder
+        type -q $e && eval $e completion fish >$XDG_CONFIG_HOME/fish/completions/$e.fish
+    end
+
+    # direnv hook
     type -q direnv && direnv hook fish >$XDG_CONFIG_HOME/fish/conf.d/direnv.fish
-    type -q kcp && kcp completion fish >$XDG_CONFIG_HOME/fish/completions/kcp.fish
+
+    # non-standard style command completion
+    type -q zellij && zellij setup --generate-completion fish >$XDG_CONFIG_HOME/fish/completions/zellij.fish
+    type -q stern && stern --completion fish >$XDG_CONFIG_HOME/fish/completions/stern.fish
 end
