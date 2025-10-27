8e951f6521a7442eca533f83981837712f26db94
pyrsson <55557230+pyrsson@users.noreply.github.com>
2025-10-27 18:04:37 +0100

    tmux stuff

--- fish/dot-config/fish/functions/setup-extras.fish
+++ fish/dot-config/fish/functions/setup-extras.fish
@@ -1,6 +1,6 @@
 function setup-extras -d 'setup extras'
     # standard style command completion
-    for e in kustomize talosctl cue kcl hcp vcluster argocd kcp kubebuilder tilt
+    for e in kustomize talosctl cue kcl hcp vcluster argocd kcp kubebuilder tilt omnictl
         type -q $e && eval $e completion fish >$XDG_CONFIG_HOME/fish/completions/$e.fish
     end
 
