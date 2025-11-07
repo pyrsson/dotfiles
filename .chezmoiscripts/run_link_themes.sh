41ea3bf7d5232146e9ea362be60fa10c9b84d375
pyrsson <55557230+pyrsson@users.noreply.github.com>
2025-11-07 09:48:47 +0100

    symlinks

--- .chezmoiscripts/run_link_themes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-ln -sfT "$HOME/.local/share/nvim/lazy/tokyonight.nvim/extras/eza/tokyonight_storm.yml" "$HOME/.local/share/eza/theme.yml"
-ln -sfT "$HOME/.local/share/nvim/lazy/tokyonight.nvim/extras/fish_themes" "$HOME/.config/fish/themes"
-ln -sfT "$HOME/.local/share/nvim/lazy/tokyonight.nvim/extras/sublime" "$HOME/.config/bat/themes"
-
-bat cache --build
