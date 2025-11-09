c0891434edc245daae6aad1a8873f9af09483a0b
pyrsson <55557230+pyrsson@users.noreply.github.com>
2025-11-09 21:45:16 +0100

    some more niri stuff

--- dot_config/fish/functions/cze.fish
+++ dot_config/fish/functions/cze.fish
@@ -1,4 +1,6 @@
 function cze --wraps "chezmoi edit"
+    set -l prev_dir (pwd)
     cd (chezmoi source-path)
     chezmoi edit --watch
+    cd $prev_dir
 end
--- dot_config/nvim/after/ftplugin/dts.lua
+++ dot_config/nvim/after/ftplugin/dts.lua
@@ -1,1 +1,5 @@
 vim.bo.commentstring = "//%s"
+vim.bo.expandtab = false
+vim.bo.shiftwidth = 4
+vim.bo.tabstop = 4
+vim.bo.softtabstop = 4
